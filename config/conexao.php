f2b1b3c19b17c7a7da3709152fe35cb3c7c8e38f
dev-modesto <gabriel_26@outlook.com.br>
2025-04-10 19:12:16 -0300

    feat: adicionar arquivo de conexão com o db

---
+++ config/conexao.php
@@ -0,0 +1,15 @@
+<?php
+    include_once 'config.php';
+    require_once BASE_PATH . '/vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $con = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
+    // echo 'conexao estabelecida com sucesso!';
+
+    header("Content-Type:text/html; charset=utf-8");
+    mysqli_query($con,"SET NAMES 'utf8'");
+    mysqli_query($con,"SET character_set_connection = utf8");
+    mysqli_query($con,"SET character_set_client = utf8");
+    mysqli_query($con,"SET character_set_results = utf8");
