cc2ef21c1b33474e2142dc4a5b33b27862b39b86
dev-modesto <gabriel_26@outlook.com.br>
2025-04-18 13:32:32 -0300

    feat: criar funções gerais

---
+++ include/funcoes/geral/geral.php
@@ -0,0 +1,35 @@
+<?php
+
+function gen_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0x0fff ) | 0x4000,
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+}
+
+/**
+ * Função para gerar uma token de 32 bites/64 caracteres e gerar um codigo de 6 digitos
+ *
+ * @return void
+ */
+function tokenCodigo() {
+    $token = random_bytes(32);
+    $token = bin2hex($token);
+    $codigoSeisDigitos = random_int(100000, 999999);
+    return ['token' =>  $token, 'codigo' => $codigoSeisDigitos];
+}
+
+function regexValidaEmail(string $email) {
+    $regexEmail = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+    return filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match($regexEmail, $email) ? true : false;
+}
+
+function removerCaracteresInput(string $valor) {
+    $valor = trim($valor);
+    $valor = stripslashes($valor);
+    $valor = htmlspecialchars($valor);
+    return $valor;
+}
