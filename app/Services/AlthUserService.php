e5d94eb7acf42291d39dd599ea29e6a6c8bf1f83
dev-modesto <gabriel_26@outlook.com.br>
2025-05-01 19:57:55 -0300

    refactor: chamar models e controller no serviço

--- app/Services/AlthUserService.php
+++ app/Services/AlthUserService.php
@@ -2,110 +2,120 @@
 
 namespace App\Services;
 use App\Controllers\AuthController;
-use mysqli;
-
-require_once '../config/base.php';
-
-include BASE_PATH . '/include/funcoes/geral/geral.php';
-include BASE_PATH . '/include/funcoes/dbQueries/geral.php';
+use App\Config\Connection;
+use App\Models\UsuarioModels;
+use App\Helpers\GeralHelper;
+use App\Helpers\DataHelper;
 
 class AlthUserService {
 
-    public static function consultaLoginService($con, $dataHoraSistema, $emailLogin, $senhaLogin) {
-        $email = removerCaracteresInput($emailLogin);
-        $senha = removerCaracteresInput($senhaLogin);
-    
-        if(regexValidaEmail($email)) {
-    
-            $sql = "SELECT * FROM tbl_usuario WHERE email = ?";
-            $sqlPrepare = mysqli_prepare($con, $sql);
-            mysqli_stmt_bind_param($sqlPrepare,'s', $email);
-            
-            if(mysqli_stmt_execute($sqlPrepare)) {
-                $returnConsulta = mysqli_stmt_get_result($sqlPrepare);
-    
-                if (mysqli_num_rows($returnConsulta)) {
-                
-                    $dados = mysqli_fetch_assoc($returnConsulta);
-                    $idUsuario = $dados['id'];
-                    $uuid = $dados['uuid'];
-                    $email = $dados['email'];
-                    $nome = $dados['nome'];
-                    $sobrenome = $dados['sobrenome'];
-                    $senhaBanco = $dados['senha'];
-                    $primeiroAcesso = $dados['primeiro_acesso'];
-                    $tentativasLogin = $dados['tentativas_login'];
-                    $status = $dados['status'];
-    
-                    if(password_verify($senha, $senhaBanco)) {
-                        // ok, senha correta. Faremos agora outras verificações...
-                        
-                        session_start();
-                        $_SESSION['idUsuario'] = $idUsuario;
-                        $_SESSION['uuid'] = $uuid;
-                        $_SESSION['email'] = $email;
-                        $_SESSION['nome'] = $nome;
-                        $_SESSION['sobrenome'] = $sobrenome;
-    
-    
-                        if ($primeiroAcesso === 'sim' && $status === 'validar') {
-                            $salvarTokenBanco = AuthController::salvarTokenBancoController($con, $dataHoraSistema, $idUsuario);
-    
-                            if ($salvarTokenBanco['alert'] == 0) {
+    public static function consultar($dados) {
 
-                                $_SESSION['token'] = $salvarTokenBanco['token'];
-    
-                                $enviarEmail = AuthController::enviarEmailController($email,"$nome $sobrenome", $salvarTokenBanco['codigo']);
-    
-                                if ($enviarEmail) {
-                                    // email enviado devemos redirecionar...
-                                    $mensagem = ['msg' => 'redirecionar para index de autenticação', 'alert' => 0, 'redirecionar' => 'autenticacao/'];
-                                    // header("location: " . BASE_URL . "/autenticacao/index.php");
-    
-                                } else {
-                                    $mensagem = ['msg' => 'Ocorreu um erro a enviar o email.', 'alert' => 1];
-                                }
-    
+        $email = GeralHelper::removerCaracteresInput($dados['email-login']);
+        $senha = GeralHelper::removerCaracteresInput($dados['senha-login']);
+        
+        if(GeralHelper::regexValidaEmail($email)) {
+
+            $usuarioModels = new UsuarioModels();
+            $dados = $usuarioModels->selecionarCriterio($email);
+
+            if (!empty($dados)) {
+
+                $idUsuario = $dados['id'];
+                $uuid = $dados['uuid'];
+                $email = $dados['email'];
+                $nome = $dados['nome'];
+                $sobrenome = $dados['sobrenome'];
+                $senhaBanco = $dados['senha'];
+                $primeiroAcesso = $dados['primeiro_acesso'];
+                $tentativasLogin = $dados['tentativas_login'];
+                $status = $dados['status'];
+
+                if(password_verify($senha, $senhaBanco)) {
+                    // ok, senha correta. Faremos agora outras verificações...
+                    
+                    session_start();
+                    $_SESSION['idUsuario'] = $idUsuario;
+                    $_SESSION['uuid'] = $uuid;
+                    $_SESSION['email'] = $email;
+                    $_SESSION['nome'] = $nome;
+                    $_SESSION['sobrenome'] = $sobrenome;
+
+                    if ($primeiroAcesso === 'sim' && $status === 'validar') {
+
+                        $idUsuario = $idUsuario;
+                        $geraToken = GeralHelper::tokenCodigo();
+                        $token = $geraToken['token'];
+                        $codigo = $geraToken['codigo'];
+                        $usado = 'nao';
+                        $arrayDatas = DataHelper::getDataHoraSistema();
+                        $dataHoraSistema = $arrayDatas['data_hora_banco'];
+                        $validade = date('Y-m-d H:i:s', strtotime('+10 minutes'));
+
+                        $dadosCompletoSalvarToken = 
+                            [
+                                'id_usuario' => $idUsuario,
+                                'token' => $token,
+                                'codigo_verificacao' => $codigo,
+                                'usado' => $usado,
+                                'created_at' => $dataHoraSistema,
+                                'validade' => $validade
+                            ]
+                        ;
+
+                        $salvarTokenBanco = AuthController::salvarTokenBancoController($dadosCompletoSalvarToken);
+
+                        if ($salvarTokenBanco['status'] == 0) {
+
+                            $_SESSION['token'] = $salvarTokenBanco['token'];
+
+                            $enviarEmail = AuthController::enviarEmailController($email,"$nome $sobrenome", $salvarTokenBanco['codigo']);
+
+                            if ($enviarEmail) {
+                                // email enviado devemos redirecionar...
+                                $mensagem = ['status' => 0, 'msg' => 'redirecionar para index de autenticação', 'alert' => 0, 'redirecionar' => 'autenticacao/'];
+                                // header("location: " . BASE_URL . "/autenticacao/index.php");
+                                // $mensagem = array_merge($mensagem, ['msgHtml' => mensagemAlertaHtml($mensagem['msg'], $mensagem['alert'])]);
+
                             } else {
-                                $mensagem = ['msg' => 'Não foi possível salvar a token.', 'alert' => 1];
+                                $mensagem = ['status' => 0, 'msg' => 'Ocorreu um erro a enviar o email.', 'alert' => 1];
                             }
 
-                            return $mensagem;
-    
-                        } elseif ($status === 'ativo') {
-                            // verificar tentativas login
-    
-                            if($tentativasLogin > 3) {  
-                                $mensagem = ['msg' => 'Tentativas de login excedidas. Tente novamente em: 3 minutos.', 'alert' => 1];
-                                return $mensagem;
-                            }
-
-                            $mensagem = ['msg' => 'Login concedido.', 'alert' => 0, 'redirecionar' => 'app/'];
-                            return $mensagem;
-    
                         } else {
-                            // exibir alerta isuario sem acesso ao sistema.
-                            $mensagem = ['msg' => 'Usuário <strong>sem acesso </strong> ao sistema.', 'alert' => 1];
+                            $mensagem = ['status' => 0, 'msg' => 'Não foi possível salvar a token.', 'alert' => 1];
+                        }
+
+                        return $mensagem;
+
+                    } elseif ($status === 'ativo') {
+                        // verificar tentativas login
+
+                        if($tentativasLogin > 3) {  
+                            $mensagem = ['status' => 0, 'msg' => 'Tentativas de login excedidas. Tente novamente em: 3 minutos.', 'alert' => 1];
                             return $mensagem;
                         }
 
+                        $mensagem = ['status' => 0, 'msg' => 'Login concedido.', 'alert' => 0, 'redirecionar' => 'app/'];
+                        return $mensagem;
+
                     } else {
-                        $mensagem = ['msg' => 'Usuário ou senha incorreta', 'alert' => 1];
+                        // exibir alerta isuario sem acesso ao sistema.
+                        $mensagem = ['status' => 0, 'msg' => 'Usuário <strong>sem acesso </strong> ao sistema.', 'alert' => 1];
                         return $mensagem;
                     }
-    
+
                 } else {
-                    $mensagem = ['msg' => 'Usuário ou senha incorreta', 'alert' => 1];
+                    $mensagem = ['status' => 0, 'msg' => 'Usuário ou senha incorreta', 'alert' => 1];
                     return $mensagem;
                 }
-    
+
             } else {
-                $mensagem = ['msg' => 'Ocorreu um erro. Não foi possível realizar o login.', 'alert' => 1];
+                $mensagem = ['status' => 0, 'msg' => 'Usuário ou senha incorreta', 'alert' => 1];
                 return $mensagem;
             }
     
         } else {
-            $mensagem = ['msg' => 'Email inválido. Favor, insira um email válido.', 'alert' => 1];
+            $mensagem = ['status' => 0, 'msg' => 'Email inválido. Favor, insira um email válido.', 'alert' => 1];
             return $mensagem;
         }
     }
