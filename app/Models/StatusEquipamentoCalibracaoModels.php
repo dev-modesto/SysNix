d3403b6d272e93f62710e0f36355f580a6887b36
dev-modesto <gabriel_26@outlook.com.br>
2025-05-03 22:12:58 -0300

    feat: criar Model consultar contagem status dos equipamentos

--- app/Models/StatusEquipamentoCalibracaoModels.php
+++ app/Models/StatusEquipamentoCalibracaoModels.php
@@ -29,4 +29,42 @@ class StatusEquipamentoCalibracaoModels {
 
     }
 
+    public function consultarContagemStatusFuncional() {
+
+        $query = 
+            "SELECT 
+                f.id AS id_status_funcional, 
+                f.nome,
+                f.cor,
+                count(c.id_status_funcional) as total 
+            FROM tbl_status_funcional f
+            LEFT JOIN tbl_equipamento_calibracao c ON (c.id_status_funcional = f.id)
+            GROUP BY f.id
+            ORDER BY f.id
+        ";
+
+        $stmt = $this->pdo->prepare($query);    
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function consultarContagemStatusUso() {
+
+        $query = 
+            "SELECT 
+                u.id AS id_status_uso,
+                u.nome,
+                u.cor,
+                COUNT(c.id_status_uso) AS total
+            FROM tbl_status_uso u
+            LEFT JOIN tbl_equipamento_calibracao c ON c.id_status_uso = u.id
+            GROUP BY u.id
+            ORDER BY u.id
+        ";
+
+        $stmt = $this->pdo->prepare($query);    
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
 }
