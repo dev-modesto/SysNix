1b172a1aaedd5bab9b619b2bc983ec887ef45e2a
dev-modesto <gabriel_26@outlook.com.br>
2025-05-20 21:49:14 -0300

    feat: autenticação de usuário segunda etapa informar a empresa

--- app/Services/AlthUserService.php
+++ app/Services/AlthUserService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 use App\Controllers\AuthController;
 use App\Config\Connection;
+use App\Controllers\UsuarioEmpresaController;
 use App\Models\UsuarioModels;
 use App\Helpers\GeralHelper;
 use App\Helpers\DataHelper;
@@ -38,72 +39,86 @@ class AlthUserService {
                     // ok, senha correta. Faremos agora outras verificações...
                     
                     session_start();
-                    $_SESSION['idUsuario'] = $idUsuario;
+                    $_SESSION['id_usuario'] = $idUsuario;
                     $_SESSION['uuid'] = $uuid;
                     $_SESSION['email'] = $email;
                     $_SESSION['nome'] = $nome;
                     $_SESSION['sobrenome'] = $sobrenome;
+                    $_SESSION['nome_completo'] = "$nome $sobrenome";
 
-                    if ($primeiroAcesso === 'sim' && $status === 'validar') {
+                    $dadosBuscarEmpresasUsuario = UsuarioEmpresaController::buscarEmpresasUsuario($idUsuario);
 
-                        $idUsuario = $idUsuario;
-                        $geraToken = GeralHelper::tokenCodigo();
-                        $token = $geraToken['token'];
-                        $codigo = $geraToken['codigo'];
-                        $usado = 'nao';
-                        $arrayDatas = DataHelper::getDataHoraSistema();
-                        $dataHoraSistema = $arrayDatas['data_hora_banco'];
-                        $validade = date('Y-m-d H:i:s', strtotime('+10 minutes'));
+                    if(!empty($dadosBuscarEmpresasUsuario)) {
+                        
+                        if ($primeiroAcesso === 'sim' && $status === 'validar') {
 
-                        $dadosCompletoSalvarToken = 
-                            [
-                                'id_usuario' => $idUsuario,
-                                'token' => $token,
-                                'codigo_verificacao' => $codigo,
-                                'usado' => $usado,
-                                'created_at' => $dataHoraSistema,
-                                'validade' => $validade
-                            ]
-                        ;
+                            $geraToken = GeralHelper::tokenCodigo();
+                            $token = $geraToken['token'];
+                            $codigo = $geraToken['codigo'];
+                            $usado = 'nao';
+                            $arrayDatas = DataHelper::getDataHoraSistema();
+                            $dataHoraSistema = $arrayDatas['data_hora_banco'];
+                            $validade = date('Y-m-d H:i:s', strtotime('+10 minutes'));
 
-                        $salvarTokenBanco = AuthController::salvarTokenBancoController($dadosCompletoSalvarToken);
+                            $dadosCompletoSalvarToken = 
+                                [
+                                    'id_usuario' => $idUsuario,
+                                    'token' => $token,
+                                    'codigo_verificacao' => $codigo,
+                                    'usado' => $usado,
+                                    'created_at' => $dataHoraSistema,
+                                    'validade' => $validade
+                                ]
+                            ;
 
-                        if ($salvarTokenBanco['status'] == 0) {
+                            $salvarTokenBanco = AuthController::salvarTokenBancoController($dadosCompletoSalvarToken);
 
-                            $_SESSION['token'] = $salvarTokenBanco['token'];
+                            if ($salvarTokenBanco['status'] == 0) {
 
-                            $enviarEmail = AuthController::enviarEmailController($email,"$nome $sobrenome", $salvarTokenBanco['codigo']);
+                                $_SESSION['token'] = $salvarTokenBanco['token'];
 
-                            if ($enviarEmail) {
-                                // email enviado devemos redirecionar...
-                                $mensagem = ['status' => 0, 'msg' => 'redirecionar para index de autenticação', 'alert' => 0, 'redirecionar' => 'autenticacao/'];
-                                // header("location: " . BASE_URL . "/autenticacao/index.php");
-                                // $mensagem = array_merge($mensagem, ['msgHtml' => mensagemAlertaHtml($mensagem['msg'], $mensagem['alert'])]);
+                                $enviarEmail = AuthController::enviarEmailController($email,"$nome $sobrenome", $salvarTokenBanco['codigo']);
+
+                                if ($enviarEmail) {
+                                    // email enviado devemos redirecionar...
+                                    $mensagem = ['status' => 0, 'msg' => 'redirecionar para index de autenticação', 'alert' => 0, 'redirecionar' => 'autenticacao/'];
+
+                                } else {
+                                    $mensagem = ['status' => 0, 'msg' => 'Ocorreu um erro a enviar o email.', 'alert' => 1];
+                                }
 
                             } else {
-                                $mensagem = ['status' => 0, 'msg' => 'Ocorreu um erro a enviar o email.', 'alert' => 1];
+                                $mensagem = ['status' => 0, 'msg' => 'Não foi possível salvar a token.', 'alert' => 1];
                             }
 
+                            return $mensagem;
+
+                        } elseif ($status === 'ativo') {
+                            if($tentativasLogin > 3) {  
+                                $mensagem = ['status' => 0, 'msg' => 'Tentativas de login excedidas. Tente novamente em: 3 minutos.', 'alert' => 1];
+                                return $mensagem;
+                            }
+
+                            if ($acessoEmpresa === 1) {
+                                $_SESSION['id_empresa'] = $dadosBuscarEmpresasUsuario[0]['id'];    
+                                $_SESSION['uuid_empresa'] = $dadosBuscarEmpresasUsuario[0]['uuid'];    
+                                $_SESSION['nome_fantasia_empresa'] = $dadosBuscarEmpresasUsuario[0]['nome_fantasia'];
+                                $mensagem = ['status' => 0, 'msg' => 'Login concedido.', 'alert' => 0, 'redirecionar' => 'app/'];
+                            } 
+                            
+                            if ($acessoEmpresa > 1) {
+                                $mensagem = ['status' => 0, 'msg' => 'Login concedido.', 'alert' => 0, 'redirecionar' => 'login/segundaEtapa.php'];
+                            } 
+                            
+                            return $mensagem;
+
                         } else {
-                            $mensagem = ['status' => 0, 'msg' => 'Não foi possível salvar a token.', 'alert' => 1];
-                        }
-
-                        return $mensagem;
-
-                    } elseif ($status === 'ativo') {
-                        // verificar tentativas login
-
-                        if($tentativasLogin > 3) {  
-                            $mensagem = ['status' => 0, 'msg' => 'Tentativas de login excedidas. Tente novamente em: 3 minutos.', 'alert' => 1];
+                            $mensagem = ['status' => 0, 'msg' => 'Usuário <strong>sem acesso </strong> ao sistema.', 'alert' => 1];
                             return $mensagem;
                         }
 
-                        $mensagem = ['status' => 0, 'msg' => 'Login concedido.', 'alert' => 0, 'redirecionar' => 'app/'];
-                        return $mensagem;
-
                     } else {
-                        // exibir alerta isuario sem acesso ao sistema.
-                        $mensagem = ['status' => 0, 'msg' => 'Usuário <strong>sem acesso </strong> ao sistema.', 'alert' => 1];
+                        $mensagem = ['status' => 0, 'msg' => 'Empresa inativa. Usuário <strong>sem acesso </strong> ao sistema.', 'alert' => 1];
                         return $mensagem;
                     }
 
