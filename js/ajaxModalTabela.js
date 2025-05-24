c2ab14b3201d2fa6d4409bb1a367f1e52ed83feb
dev-modesto <gabriel_26@outlook.com.br>
2025-05-23 22:18:40 -0300

    feat: criar funcao modal ação ajax controller

--- js/ajaxModalTabela.js
+++ js/ajaxModalTabela.js
@@ -23,3 +23,38 @@ function ajaxModalTabelaAcaoExcluir(publickey, controller, acao, baseUrl) {
 
     });
 }
+
+function ajaxControllerModalAcao(formSubmit, controller, acao, baseUrl) {
+
+    $(document).ready(function () {
+
+        $(document).on('submit', `${formSubmit}`, function (e) {
+            e.preventDefault();
+
+            if (!this.checkValidity()) {
+                this.reportValidity();
+                return;
+            }
+
+            const formData = new FormData(this);
+            formData.append('controller', controller)
+            formData.append('acao',acao);
+
+            $.ajax({
+                type: "POST",
+                url: `${baseUrl}/public/ajaxController.php`,
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    window.location.href = `${baseUrl}/${response.data.redirecionar}?msg=${encodeURIComponent(response.data.msg)}&alert=${response.data.alert}`;
+                }
+            });
+        });
+
+        $(document).on('click', '.btn-submit-modal', function () {
+            $(`${formSubmit}`).trigger('submit');
+        });
+
+    });
+}
