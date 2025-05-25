72812f1f39e9e25688cd4fa6401ce1f41f1cf727
dev-modesto <gabriel_26@outlook.com.br>
2025-05-25 16:29:08 -0300

    feat: criar método do serviço para atualizar equipamento de calibração

--- app/Services/EquipamentoCalibracaoService.php
+++ app/Services/EquipamentoCalibracaoService.php
@@ -87,4 +87,51 @@ class EquipamentoCalibracaoService
     
 
     }
+
+    public function atualizarEquipamento($dados) {
+
+        $publicKey = $dados['public-key'];
+
+        $uuidHelper = new UuidHelper();
+        $retornoUuidHelper = $uuidHelper->enviaUuidBuscaDados('tbl_equipamento_calibracao', $publicKey);
+        $idEquipamento = $retornoUuidHelper['id'];
+
+        $retornoUuidHelperStatusFuncional = $uuidHelper->enviaUuidBuscaDados('tbl_status_funcional', $dados['public-key-status-funcional']);
+        $idStatusFuncional = $retornoUuidHelperStatusFuncional['id'];
+
+        $retornoUuidHelperStatusUso = $uuidHelper->enviaUuidBuscaDados('tbl_status_uso', $dados['public-key-status-uso']);
+        $idStatusUso = $retornoUuidHelperStatusUso['id'];
+
+        $nomeIdentificador = removerCaracteresInput($dados['nome-identificador']);
+
+        $dataHelper = DataHelper::getDataHoraSistema();
+        $dadosAtualizar = [
+            'nome_identificador' => $nomeIdentificador,
+            'descricao' => $dados['descricao'],
+            'modelo' => $dados['modelo'],
+            'fabricante' => $dados['fabricante'],
+            'serie' => $dados['numero-serie'],
+            'resolucao' => $dados['resolucao'],
+            'faixa_uso' => $dados['faixa-uso'],
+            'dt_ultima_calibracao' => $dados['data-ultima-calibracao'],
+            'numero_certificado' => $dados['numero-certificado'],
+            'dt_calibracao_previsao' => $dados['data-previsao-calibracao'],
+            'ei_15a25_n' => $dados['ei-15a25-n'],
+            'ei_2a8' => $dados['ei-2a8'],
+            'ei_15a25' => $dados['ei-15a25'],
+            'updated_at' => $dataHelper['data_hora_banco'],
+            'id_status_funcional' => $idStatusFuncional,
+            'id_status_uso' => $idStatusUso,
+        ];
+
+        $cadastrarEquipamentos = new EquipamentoCalibracaoModel();
+        $retorno = $cadastrarEquipamentos->atualizar($idEquipamento, $dadosAtualizar);
+
+        if ($retorno == 0) {
+            return ['status' => 1, 'msg' => 'Não foi possível atualizar o equipamento.', 'alert' => 1, 'redirecionar' => 'apps/operacional/calibracaoEquipamentos/'];
+        }
+
+        return ['status' => 0, 'msg' => "Equipamento $nomeIdentificador atualizado com sucesso.", 'alert' => 0, 'redirecionar' => 'apps/operacional/calibracaoEquipamentos/'];
+        
+    }
 }
