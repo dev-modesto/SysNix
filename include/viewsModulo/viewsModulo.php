1e33d273547a5398ccbddd6404f4788263cd50f0
gabriel.modesto <gabriel_26@outlook.com.br>
2025-05-27 14:50:01 -0300

    style: ajustar para 4 colunas de cards nos módulos

--- include/viewsModulo/viewsModulo.php
+++ include/viewsModulo/viewsModulo.php
@@ -5,7 +5,7 @@
 <style>
     .container-cards-telas {
         display: grid;
-        grid-template-columns: repeat(6, 1fr);
+        grid-template-columns: repeat(4, 1fr);
         gap: 20px;
     }
 
