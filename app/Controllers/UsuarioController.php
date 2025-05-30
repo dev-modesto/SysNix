b577636bad184e84137ec15b578d6f9793e900b0
dev-modesto <gabriel_26@outlook.com.br>
2025-05-30 09:03:38 -0300

    feat: criar controller usuario

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Services\Usuario\UsuarioService;
+
 class UsuarioController
 {
 
     public static function cadastrar(array $dados) {
 
+        $usuarioService = new UsuarioService();
+        $dadosUsuarioService = $usuarioService->cadastrarUsuario($dados);
+
+        return $dadosUsuarioService;
     }
 
 }
