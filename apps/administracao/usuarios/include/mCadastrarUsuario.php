fb17c5a5c398f41eff616a6706ac3d6347746bd6
dev-modesto <gabriel_26@outlook.com.br>
2025-05-30 09:06:38 -0300

    feat: adicionar select empresa ao modal cadastrar usuário

--- apps/administracao/usuarios/include/mCadastrarUsuario.php
+++ apps/administracao/usuarios/include/mCadastrarUsuario.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\Empresa\EmpresaController;
 use App\Controllers\StatusEquipamentoCalibracaoController;
 include_once '../../../../config/base.php';
 
@@ -25,12 +26,32 @@ include_once '../../../../config/base.php';
             </div>
         </div>
 
+        <div class="col-md-12 mb-4">
+            <label class="font-1-s" for="empresa">Empresa <em>*</em></label><br>
+            <select class="form-select select-empresa" name="public-key-empresa" id="empresa" required>
+                <option value="" selected>Escolha uma empresa</option>
+                <?php
+
+                    $dadosEmpresas = EmpresaController::buscarEmpresas();
+
+                    foreach ($dadosEmpresas as $valor) {
+                        echo <<<HTML
+                            <option value="{$valor['uuid']}">{$valor['nome_fantasia']}</option>
+                        HTML;
+                    }
+                    
+                ?>
+            </select>                                        
+        </div>
+
         <div class="row mb-4">
             <div class="col-md-6">
                 <label class="font-1-s" for="senha">Senha <em>*</em></label><br>
                 <input class="form-control" type="password" name="senha" id="senha" required>
             </div>
         </div>
+
+ 
     </form>
 </div>
 
