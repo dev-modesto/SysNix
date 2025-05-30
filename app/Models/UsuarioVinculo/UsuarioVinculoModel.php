7c5e3d1ae9e16e6efb7ee605d27796e8055ec722
dev-modesto <gabriel_26@outlook.com.br>
2025-05-30 09:06:51 -0300

    feat: criar model usuario vinculo

---
+++ app/Models/UsuarioVinculo/UsuarioVinculoModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\UsuarioVinculo;
+
+use App\Config\Connection;
+use PDO;
+
+class UsuarioVinculo
+{
+
+    protected PDO $pdo;
+
+    public function __construct() {
+        $this->pdo = Connection::getInstance();
+    }
+
+    public function vincularUsuarioEmpresa(array $dados) {
+
+        $query = 
+            "INSERT INTO tbl_usuario_empresa
+            (
+                id_empresa, 
+                id_usuario, 
+                created_at
+            ) VALUES (
+                :id_empresa,
+                :id_usuario,
+                :created_at
+            )
+        ";
+
+        $stmt = $this->pdo->prepare($query);
+
+        foreach ($dados as $chave => $valor) {
+            $stmt->bindValue(":$chave", $valor);
+        }
+        $stmt->execute();
+        return $this->pdo->lastInsertId();
+    }
+
+}
