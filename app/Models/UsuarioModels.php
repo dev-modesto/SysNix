b30ee261c378e20dd68abfbc9a7fbeb586897732
dev-modesto <gabriel_26@outlook.com.br>
2025-06-04 21:13:45 -0300

    feat: criar método da model selecionar usuario

--- app/Models/UsuarioModels.php
+++ app/Models/UsuarioModels.php
@@ -13,6 +13,36 @@ class UsuarioModels
         $this->pdo = Connection::getInstance();
     }
 
+    public function selecionar() {
+        $query = 
+            "SELECT
+                u.id,
+                u.uuid,
+                u.email,
+                u.nome,
+                u.sobrenome,
+                u.senha,
+                u.foto,
+                u.primeiro_acesso,
+                u.tentativas_login,
+                u.status,
+                GROUP_CONCAT(e.nome_fantasia SEPARATOR ' | ') AS empresas,
+                GROUP_CONCAT(e.status SEPARATOR ', ') AS status_empresas
+            FROM tbl_usuario u
+            INNER JOIN tbl_usuario_empresa ue ON ue.id_usuario = u.id
+            INNER JOIN tbl_empresa e ON ue.id_empresa = e.id
+            GROUP BY
+                u.id, u.uuid, u.email, u.nome, u.sobrenome, u.senha,
+                u.foto, u.primeiro_acesso, u.tentativas_login, u.status
+        ";
+
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+        $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $dados;
+
+    }
+
     public function selecionarCriterio($email) {
         $query = 
             "SELECT 
@@ -77,19 +107,19 @@ class UsuarioModels
     }
 
     public function atualizarUsuario($dados) {
-        $query = 
-            "UPDATE 
-                tbl_usuario 
-            SET 
-                email = :email, 
-                nome = :nome, 
-                sobrenome = :sobrenome, 
-                updated_at = :updated_at
-            WHERE 
-                id = :id
-        ";
+        $id = $dados['id'];
+        unset($dados['id']);
+
+        $campos = [];
+
+        foreach ($dados as $coluna => $valor) {
+            $campos[] = "{$coluna} = :{$coluna}";
+        }
+
+        $query = "UPDATE tbl_usuario SET " . implode(',', $campos) . " WHERE id = :id";
 
         $stmt = $this->pdo->prepare($query);
+        $dados['id'] = $id;
 
         foreach ($dados as $chave => $valor) {
             $stmt->bindValue(":$chave", $valor);
