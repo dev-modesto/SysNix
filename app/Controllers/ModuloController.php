a31f2e88ac3927655efca8357107273a8591bf27
gabriel.modesto <gabriel_26@outlook.com.br>
2025-06-06 14:36:40 -0300

    feat: adicionar novos métodos ao controller de modulo

--- app/Controllers/ModuloController.php
+++ app/Controllers/ModuloController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Helpers\UuidHelper;
 use App\Models\ModuloModel;
+use App\Services\Modulo\ModuloService;
 
 class ModuloController
 {
@@ -14,6 +16,34 @@ class ModuloController
 
         return $arrayDadosModulo;
 
+    }
+
+    public static function selecionar() {
+        $modulo = new ModuloModel();
+        $dadosReturn = $modulo->selecionarModulos();
+        return $dadosReturn;
+    }
+
+    public static function selecionaModuloUuid($uuidModulo) {
+        $uuidHelper = new UuidHelper();
+        $dadosUuidHelper = $uuidHelper->enviaUuidBuscaDados('tbl_modulo', $uuidModulo);
+
+        return $dadosUuidHelper;
+    }
+
+    public static function cadastrar(array $dados) {
+
+        $moduloService = new ModuloService();
+        $dadosModuloService = $moduloService->cadastrarModulo($dados);
+
+        return $dadosModuloService;
+    }
+
+    public static function atualizar($dados) {
+        $moduloService = new ModuloService();
+        $dadosModuloService = $moduloService->atualizarModulo($dados);
+
+        return $dadosModuloService;
     }
 
 }
