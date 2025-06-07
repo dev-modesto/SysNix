9bf9fcd4b148e9359cc99f0271119f7eaf545a4d
dev-modesto <gabriel_26@outlook.com.br>
2025-06-07 17:27:14 -0300

    feat: adicionar métodos ao controller view

--- app/Controllers/ViewController.php
+++ app/Controllers/ViewController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Helpers\UuidHelper;
 use App\Models\ViewModel;
+use App\Services\View\ViewService;
 
 class ViewController
 {
@@ -14,6 +16,34 @@ class ViewController
 
         return $dadosReturn;
 
+    }
+
+    public static function selecionar() {
+        $viewModel = new ViewModel();
+        $dadosReturn = $viewModel->selecionarTelas();
+        return $dadosReturn;
+    }
+
+    public static function selecionaTelaUuid($uuid) {
+        $uuidHelper = new UuidHelper();
+        $dadosUuidHelper = $uuidHelper->enviaUuidBuscaDados('tbl_tela', $uuid);
+
+        return $dadosUuidHelper;
+    }
+
+    public static function cadastrar(array $dados) {
+
+        $viewService = new ViewService();
+        $dadosReturnCadastro = $viewService->cadastrarTela($dados);
+
+        return $dadosReturnCadastro;
+    }
+
+    public static function atualizar($dados) {
+        $viewService = new ViewService();
+        $dadosReturnCadastro = $viewService->atualizarTela($dados);
+
+        return $dadosReturnCadastro;
     }
 
 }
